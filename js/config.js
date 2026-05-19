f731c7223afedc325b010575f50ffbf535aa0f99
Kolt <144575750+koltluo@users.noreply.github.com>
2026-05-19 15:13:01 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -14,7 +14,7 @@ const PASSWORD_CONFIG = {
 // 网站信息配置
 const SITE_CONFIG = {
     name: 'LibreTV',
-    url: 'https://libretv.is-an.org',
+    url: 'https://tv.nav8.top',
     description: '免费在线视频搜索与观看平台',
     logo: 'image/logo.png',
     version: '1.0.3'
